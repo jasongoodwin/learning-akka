16ff1506ffc5767a278f32a368cff601c08aff91
Jason Goodwin <jay.michael.goodwin@gmail.com>
2015-12-20 16:09:04 -0500

    Update ScalaPongActor.scala

--- ch2/futures-examples-scala/src/main/scala/pong/ScalaPongActor.scala
+++ ch2/futures-examples-scala/src/main/scala/pong/ScalaPongActor.scala
@@ -3,7 +3,6 @@ package pong
 import akka.actor.{Actor, Status}
 
 class ScalaPongActor extends Actor {
-  self
   override def receive: Receive = {
     case "Ping" => sender() ! "Pong"
     case _ => sender() ! Status.Failure(new Exception("unknown message"))
